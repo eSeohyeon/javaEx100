84aa5578d3d2370b26293a1b9deb9b9aa1a5cb2e
eSeohyeon <leeseohyeon1203@gmail.com>
2023-11-07 15:37:56 +0900

    22~25

---
+++ ex100_24.java
@@ -0,0 +1,18 @@
+package java_ex100;
+import java.util.Scanner;
+
+public class ex100_24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int x=scan.nextInt();
+		int y=scan.nextInt();
+		if(y>=x)
+			System.out.println(1);
+		else
+			System.out.println(0);
+
+	}
+
+}
---
+++ ex100_25.java
@@ -0,0 +1,19 @@
+package java_ex100;
+import java.util.Scanner;
+
+public class ex100_25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int x=scan.nextInt();
+		int y=scan.nextInt();
+		
+		if(x!=y)
+			System.out.println(1);
+		else
+			System.out.println(0);
+
+	}
+
+}
